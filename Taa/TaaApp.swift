f9c8844f02003c0aaceea537da704ff9f7b488ba
Ochanyoro <wadaoo0223@icloud.com>
2022-07-01 07:32:44 +0900

    Initial Commit

---
+++ Taa/TaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaaApp.swift
+//  Taa
+//
+//  Created by 大和田一裕 on 2022/07/01.
+//
+
+import SwiftUI
+
+@main
+struct TaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
