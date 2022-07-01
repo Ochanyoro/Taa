bae46be8632edd7ca0d596693eb9280875838384
Ochanyoro <wadaoo0223@icloud.com>
2022-07-01 10:29:29 +0900

    [create] TabView(ContentView)

--- Taa/ContentView.swift
+++ Taa/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    init(){
+        //TabViewの背景色の設定（黒色）
+        UITabBar.appearance().backgroundColor = UIColor.black
+        UITabBar.appearance().unselectedItemTintColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.7)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        TabView{
+            Text("main view")
+                .tabItem{
+                    Image(systemName: "person")
+                    Text("ホーム")
+                }
+            Text("main view")
+                .tabItem{
+                    Image(systemName: "calendar")
+                    Text("カレンダー")
+                }
+                //.tag(0)
+            
+            Text("main view")
+                .tabItem{
+                    Image(systemName: "magnifyingglass")
+                    Text("発見")
+                }
+                // .tag(1)
+            
+            Text("main view")
+                .tabItem(){
+                    Image(systemName: "leaf")
+                    Text("お気に入り")
+                }
+               // .tag(2)
+            Text("gg")
+                .tabItem {
+                    Image(systemName: "message.circle")
+                    Text("チャット")
+                }
+        }
+        
+        .accentColor(Color.white)
     }
 }
 
